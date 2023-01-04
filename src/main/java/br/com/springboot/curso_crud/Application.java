56bd70c5bd5175a45347a043174a5a84b7c17ae1
eversonsba <everson.paulino@gmail.com>
2023-01-03 23:44:12 -0300

    criando conexão com banco, repositórios e tentando salvar

--- src/main/java/br/com/springboot/curso_crud/Application.java
+++ src/main/java/br/com/springboot/curso_crud/Application.java
@@ -2,11 +2,13 @@ package br.com.springboot.curso_crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 /**
  *
  * Spring Boot application starter class
  */
+@EntityScan(basePackages = "br.com.springboot.curso_crud.model")
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
